412fa6a2eddaf1f9d5ec5383123341fb179a8c98
Rasmus Enhörning <rasmus.enhorning@seb.se>
2019-10-15 10:45:19 +0200

    chore: fix broken tests

--- projects/seb-ng-wizard-demo/src/app/app.component.spec.ts
+++ projects/seb-ng-wizard-demo/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
@@ -5,6 +6,7 @@ describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
