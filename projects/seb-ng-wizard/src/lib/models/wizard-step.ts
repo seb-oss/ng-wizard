4b5e41718c34d4d6e7e5efbe0a14bd70ad7a3640
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-05-17 12:37:51 +0200

    fix: update interfaces

--- projects/seb-ng-wizard/src/lib/models/wizard-step.ts
+++ projects/seb-ng-wizard/src/lib/models/wizard-step.ts
@@ -1,24 +1,41 @@
 import { Route } from '@angular/router';
 export declare type WizardSteps = WizardStep[];
 export declare type WizardControls = WizardControl[];
+
+export interface WizardStepConfigs {
+  [key: string]: WizardStepConfig;
+}
+
+export interface WizardStepConfig {
+  data: WizardStepData;
+  path: string;
+  children?: {
+    [key: string]: WizardStepConfig;
+  };
+}
 export interface WizardStep extends Route {
   data?: WizardStepData;
   children?: WizardSteps;
 }
 
 export interface WizardStepData {
+  number?: number;
   heading: string; // step heading
+  pageHeading?: string; // step heading
   hideNavigation?: boolean;
   controls: WizardControl[];
+  state?: 'success' | 'warning' | 'danger' | 'info';
   secondaryContent?: {
     component: any;
     data?: { [key: string]: any };
     class?: string;
   };
+  subSteps?: string[];
 }
 
 export interface WizardControl {
   name?: string; // label for action
+  textKey?: string; // text key used for translations
   path?: string; // path where action takes user (optional)
   class?: string; // class to be added to control (optional)
   type: 'next' | 'prev' | 'cancel' | 'save' | 'close'; // type of action (determines icon and can be used for custom triggers)
