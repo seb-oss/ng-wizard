f891c386dd486531ccab9cacdda5f9c4c6f79b5b
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-05-17 12:42:02 +0200

    refactor: update module

--- projects/seb-ng-wizard/src/lib/seb-ng-wizard.module.ts
+++ projects/seb-ng-wizard/src/lib/seb-ng-wizard.module.ts
@@ -1,23 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { InjectionToken, ModuleWithProviders, NgModule } from '@angular/core';
+import { Injectable, InjectionToken, ModuleWithProviders, NgModule, Provider } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/pro-regular-svg-icons/faArrowLeft';
 import { faArrowRight } from '@fortawesome/pro-regular-svg-icons/faArrowRight';
 import { faPrint } from '@fortawesome/pro-regular-svg-icons/faPrint';
 import { faSave } from '@fortawesome/pro-regular-svg-icons/faSave';
+import { Observable, of } from 'rxjs';
 import { ControlsComponent } from './components/controls/controls.component';
 import { LeftNavigationComponent } from './components/left-navigation/left-navigation.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { WizardComponent } from './components/wizard/wizard.component';
-import { SecondaryContentComponent, WizSecondaryContentDirective } from './directives/secondary-content.directive';
+import { WizSecondaryContentDirective } from './directives/secondary-content.directive';
 import { WizardControlService } from './services/wizard-control.service';
+
+export abstract class WizardTranslations {
+  abstract translations$: Observable<any>;
+}
+
+@Injectable()
+export class DefaultWizardTranslations implements WizardTranslations {
+  constructor() {}
+  translations$: Observable<any> = of({
+    next: 'Next',
+    prev: 'Back',
+    save: 'Save',
+    close: 'Close',
+  });
+}
 
 interface SebNgWizardConfig {
   translations: any;
+  markPassedAsSuccess: boolean;
 }
 
 const defaultConfig = {
+  markPassedAsSuccess: true,
   translations: {
     en: {
       next: 'Next',
@@ -35,6 +53,7 @@ const defaultConfig = {
 };
 
 export const SebNgWizardConfigService = new InjectionToken<SebNgWizardConfig>('SebNgWizardConfig');
+export const SebNgWizardTranslateService = new InjectionToken<Provider>('AbstractLibTranslations');
 
 @NgModule({
   declarations: [
@@ -53,7 +72,7 @@ export class SebNgWizardModule {
     // add icons that should be available in the app/module
     library.addIcons(faArrowRight, faArrowLeft, faSave, faPrint);
   }
-  static forRoot(config?: SebNgWizardConfig): ModuleWithProviders<SebNgWizardModule> {
+  static forRoot(config?: SebNgWizardConfig, translations?: Provider): ModuleWithProviders<SebNgWizardModule> {
     return {
       ngModule: SebNgWizardModule,
       providers: [
@@ -62,6 +81,7 @@ export class SebNgWizardModule {
           provide: SebNgWizardConfigService,
           useValue: config || defaultConfig,
         },
+        translations || { provide: WizardTranslations, useClass: DefaultWizardTranslations },
       ],
     };
   }
