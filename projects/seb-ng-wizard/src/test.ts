801356e85591d0a3feeee2e46da13375efa6aa29
Hjalmers <opensource@rhj.se>
2023-04-24 20:41:44 +0200

    chore(deps): bump to angular 15

--- projects/seb-ng-wizard/src/test.ts
+++ projects/seb-ng-wizard/src/test.ts
@@ -6,13 +6,7 @@ import 'core-js/es7/reflect';
 import 'zone.js';
 import 'zone.js/testing';
 
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
   teardown: { destroyAfterEach: false },
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
